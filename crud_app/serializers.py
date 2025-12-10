c6585f9ad791a2aba24fb3b3f65bc5e861bb3adb
idrees2025 <shahjahanidrees449@gmail.com>
2025-12-10 06:47:02 +0000

    fix error

--- crud_app/serializers.py
+++ crud_app/serializers.py
@@ -4,7 +4,7 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model=Student
-        fields=['name','age','section']
+        fields=['id','name','age','section']
 
     def create(self, validated_data):
         return Student.objects.create(**validated_data)
@@ -13,4 +13,5 @@ class StudentSerializer(serializers.ModelSerializer):
         instance.name=validated_data.get('name',instance.name)
         instance.age=validated_data.get('age',instance.age)
         instance.section=validated_data.get('section',instance.section)
+        instance.save()
         return instance
